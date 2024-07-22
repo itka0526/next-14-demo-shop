5101376980a9db46b0f223cc70c69263b6c12c25
itka0526 <itka0526@gmail.com>
2024-07-23 03:34:04 +0800

    sqlite -> postgresql

--- prisma/migrations/20240722065828_123/migration.sql
+++
@@ -1,7 +0,0 @@
--- CreateTable
-CREATE TABLE "Thumnail" (
-    "id" TEXT NOT NULL PRIMARY KEY,
-    "imageUrl" TEXT NOT NULL DEFAULT '/placeholder.svg',
-    "productId" INTEGER,
-    CONSTRAINT "Thumnail_productId_fkey" FOREIGN KEY ("productId") REFERENCES "Product" ("id") ON DELETE SET NULL ON UPDATE CASCADE
-);
